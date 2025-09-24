35253be823f797f2ebde6ade4506ee3ccbec78f3
mila0408 <159796730+mila0408@users.noreply.github.com>
2025-09-24 21:24:49 +0300

    Add files via upload

---
+++ PSbFM/PSbFM/Category.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSbFM
+{
+    public class Category
+    {
+        public List<string> CategoryList = new List<string>();
+        public string name = "Shopping";
+        public Category() {
+            CategoryList.Append("Food");
+            CategoryList.Append($"Transport");
+            CategoryList.Append($"Money transfer");
+            CategoryList.Append($"Shopping");
+            CategoryList.Append($"Sub");
+            CategoryList.Append($"Health");
+            CategoryList.Append($"Entertainment and relaxation");
+        }
+         public void ChangeCategory(int index)
+        {
+           name = CategoryList[index-1];
+        } 
+        public void AddCategory( string newCategory)
+        {
+            CategoryList.Append($"{newCategory}"); 
+        }
+
+
+
+
+
+    }
+        
+    
+}
